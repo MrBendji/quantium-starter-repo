019d3886d27215c53f441a1d5dfbcd0badf9a26f
MrBendji <phenixdark27@gmail.com>
2023-06-04 01:01:28 +0100

    Data dashbord inter

---
+++ visu.py
@@ -0,0 +1,23 @@
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
+filtered_data = pd.read_csv('./merged_data.csv')
+
+# Create the Dash app
+app = Dash(__name__)
+
+# Define the app layout
+app.layout = html.Div(
+    children=[
+        html.H1('Sales Visualization'),
+        dcc.Graph(
+            id='sales-chart',
+            figure=px.line(filtered_data, x='date', y='Sales', title='Sales Data')
+        )
+    ]
+)
+
+# Run the app
+if __name__ == '__main__':
+    app.run_server(debug=True)
